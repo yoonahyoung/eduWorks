1db5a3fd776d2486243c5b268c01672299bba89c
ssuminee <rntnalseee@gmail.com>
2022-09-07 11:24:43 +0900

    add : attachment 파일 추가
    author : sumin
    
    add Attachment.java

---
+++ src/main/java/com/finalProject/eduWorks/common/model/vo/Attachment.java
@@ -0,0 +1,27 @@
+package com.finalProject.eduWorks.common.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Attachment {
+	
+	private int atNo;
+	private int atRefBNo;
+	private String atCategory;
+	private String atOrginName;
+	private String atChangeName;
+	private String atPath;
+	private Date atUploadDate;
+	private String atStatus;
+	
+}
