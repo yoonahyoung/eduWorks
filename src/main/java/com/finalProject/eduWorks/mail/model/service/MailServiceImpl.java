a0e6c2683c583fee533efd5b66415c1bd1fdf299
key1018 <103404357+key1018@users.noreply.github.com>
2022-09-07 12:11:01 +0900

    add : 메일에 대한 클래스 및 인터페이스 추가
    add : 메일에 대한 클래스 및 인터페이스 추가
    author : EunYoung
    
    - add eduWorks.mail.model.vo.Tag.java
    - add eduWorks.mail.model.vo.Spam.java
    - add eduWorks.mail.model.vo.Mail.java
    - add eduWorks.mail.model.vo.MailStatus.java
    
    - add eduWorks.mail.model.service.MailService.java
    - add eduWorks.mail.model.service.MailServiceImpl.java
    
    - add eduWorks.mail.model.dao.MailDao.java
    - add eduWorks.mail.controller.MailController.java

---
+++ src/main/java/com/finalProject/eduWorks/mail/model/service/MailServiceImpl.java
@@ -0,0 +1,25 @@
+package com.finalProject.eduWorks.mail.model.service;
+
+import org.springframework.stereotype.Service;
+
+import com.finalProject.eduWorks.mail.model.vo.Mail;
+
+@Service
+public class MailServiceImpl implements MailService {
+
+	@Override
+	public int insertMail(Mail m) {
+		return 0;
+	}
+
+	@Override
+	public int replyMail(Mail m) {
+		return 0;
+	}
+
+	@Override
+	public int insertMailMe(Mail m) {
+		return 0;
+	}
+
+}
