98c0e9c42e721e06d2c19d41382a3cd6cf22bfb4
Seojuwon11 <juwon0085@naver.com>
2022-09-07 17:12:31 +0900

    add : 부서,직급 VO클래스
    author : juwon
    add Department.java
    add Job.java

---
+++ src/main/java/com/finalProject/eduWorks/member/model/vo/Department.java
@@ -0,0 +1,17 @@
+package com.finalProject.eduWorks.member.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Department {
+	private String deptCode;
+	private String deptName;
+}
---
+++ src/main/java/com/finalProject/eduWorks/member/model/vo/Job.java
@@ -0,0 +1,17 @@
+package com.finalProject.eduWorks.member.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Job {
+	private String jobCode;
+	private String jobName;
+}
